2faa37c5b7a4f4ee0d186175bcb1163b0126ea4b
Gabriella Conde <conde.gabriella.r@gmail.com>
2023-11-12 05:13:14 -0500

    Remove template comments

--- src/Graph.h
+++ src/Graph.h
@@ -4,11 +4,9 @@
 
 class Graph {
     private:
-        //Think about what member variables you need to initialize
         std::map<std::string, std::set<std::string>> AdjacencyList {};
 
     public:
-        //Think about what helper functions you will need in the algorithm
         void InsertEdge(std::string from, std::string to);
         void PageRank(int n);
 };
