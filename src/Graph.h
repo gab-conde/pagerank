d877b03be34292bf125fb693c3e3a9ba1a334442
Gabriella Conde <conde.gabriella.r@gmail.com>
2023-11-12 04:57:15 -0500

    Add data members

--- src/Graph.h
+++ src/Graph.h
@@ -1,6 +1,11 @@
+#include <string>
+#include <map>
+#include <set>
+
 class Graph {
     private:
         //Think about what member variables you need to initialize
+        std::map<std::string, std::set<std::string>> AdjacencyList {};
 
     public:
         //Think about what helper functions you will need in the algorithm
