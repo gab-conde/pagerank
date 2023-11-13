069461106ce25e5a55aa6aa91ae51043b8a2de4c
Gabriella Conde <conde.gabriella.r@gmail.com>
2023-11-13 04:06:27 -0500

    Update insertion

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -8,6 +8,13 @@
 void Graph::InsertEdge(std::string from, std::string to) {
 
     AdjacencyList[to].emplace(std::make_pair(from, 0.0));
+
+    if (Outdegrees.find(to) != Outdegrees.end()) {
+        Outdegrees[to]++;
+    }
+    else {
+        Outdegrees.emplace(to, 1);
+    }
 }
 
 // Perform p power iterations and display results
--- src/Graph.h
+++ src/Graph.h
@@ -5,6 +5,7 @@
 class Graph {
     private:
         std::map<std::string, std::set<std::pair<std::string, float>>> AdjacencyList {};
+        std::map<std::string, int> Outdegrees {};
 
     public:
         void InsertEdge(std::string from, std::string to);
