26fee756762c6f0151cb40bcaabdda1c2340842c
Gabriella Conde <conde.gabriella.r@gmail.com>
2023-11-13 04:03:29 -0500

    Update comments

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -10,6 +10,5 @@ void Graph::InsertEdge(std::string from, std::string to) {
     AdjacencyList[to].emplace(std::make_pair(from, 0.0));
 }
 
-// prints the PageRank of all pages after p powerIterations in ascending alphabetical order
-//  of webpages and rounding rank to two decimal places
-void Graph::PageRank(int n){ }
+// Perform p power iterations and display results
+void Graph::PageRank(int n){}
